1d5ebb0d47771ee0cf7a5de306af12198f14dd6e
Bruxar <larancibia@utem.cl>
2024-02-08 19:38:52 -0300

    terminado el listado

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,19 @@
 <?php
 
+    //Importar conexion
+    require '../includes/config/database.php';
+    $db = conectarDB();
+
+    //Escribir query
+    $query = "SELECT * FROM propiedades";
+
+    //Consuyltar BD
+    $resultadoConsulta = mysqli_query($db, $query);
+
+    //Muestra mensaje condicional
     $resultado = $_GET['resultado'] ?? null;
+
+    //Incluye template
     require '../includes/funciones.php';
     incluirTemplate('header');
 ?>
@@ -12,8 +25,39 @@
         <?php endif; ?>
 
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Titulo</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+
+            <tbody> <!-- Mostrar los resultados -->
+            <?php while( $propiedad = mysqli_fetch_assoc($resultadoConsulta)): ?>
+                <tr>
+                    <td><?php echo $propiedad['id']; ?></td>
+                    <td><?php echo $propiedad['titulo']; ?></td>
+                    <td> <img src="/imagenes/<?php echo $propiedad['imagen']; ?>" class="imagen-tabla"> </td>
+                    <td>$<?php echo $propiedad['precio']; ?></td>
+                    <td>
+                        <a href="" class="boton-rojo-block">Eliminar</a>
+                        <a href="" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+                <?php endwhile; ?>
+            </tbody>
+        </table>
+
     </main>
 
 <?php
+    //Cerrar conexion
+    mysqli_close($db);
+
     incluirTemplate('footer');  
 ?>
